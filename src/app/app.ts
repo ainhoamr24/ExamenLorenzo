7c3526ab97238de73bc0e093d3ad5719d0867564
ainhoa <ainhoamiret@gmail.com>
2025-11-18 07:47:21 +0100

    Preparado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Articulos} from './components/paginas/articulos/articulos';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,  Articulos],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
